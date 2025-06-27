539bc7d018cbabb8e925cd5069ec7b6e909978c2
Ole Lie-Bjelland <67302772+OleLB@users.noreply.github.com>
2025-06-28 00:39:18 +0200

    Delete waybar directory

--- waybar/active_window.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-WINDOW=$(hyprctl clients | grep "class: " | awk '{gsub("class: ", "");print}' | rofi -show window)
-echo 'window is: '
-echo $WINDOW
-if [ "$WINDOW" = "" ]; then
-    exit
-fi
-hyprctl dispatch focuswindow $WINDOW
